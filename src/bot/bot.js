171b9321f56bea344d99da35641b6d8334968814
Raj Mahajan <raj.maha1947@gmail.com>
2024-05-28 21:58:55 +0200

    feat: bot can play ping pong for now

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,4 +1,31 @@
+const { Client, GatewayIntentBits } = require('discord.js');
 
-module.exports = {
+require('dotenv').config();
+const token = process.env.TOKEN;
 
-}
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent,
+    ]
+});
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+});
+
+client.on("messageCreate", msg => {
+    if (msg.content === "ping") {
+        msg.reply("pong");
+    }
+});
+
+client.on('error', (error) => {
+    console.error('Client Error:', error);
+});
+
+client.login(token).catch(error => {
+    console.error('Failed to login:', error);
+});
